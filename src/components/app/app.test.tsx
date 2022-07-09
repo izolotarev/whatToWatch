6e700007ef516389ba4ecb960be176924c51867c
Igor Zolotarev <izolotarev@hotmail.com>
2022-07-09 17:24:44 +0300

    init: start movies project

---
+++ src/components/app/app.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './app';
+
+test('renders Hello, world!', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello, world!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
